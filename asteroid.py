b84205199267fab64553ea4f0104d8f7d9c0b21f
PaperDrago <charlie_shadow@hotmail.com>
2024-11-02 19:24:33 -0500

    Modified files

---
+++ asteroid.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Asteroids(CircleShape):
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+    
+    def draw():
--- main.py
+++ main.py
@@ -6,6 +6,15 @@ from constants import *
 
 def main():
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
-    
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    i = 0
+    while i == 0:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.fill((0,0,0))
+        pygame.display.flip()
+
 if __name__ == "__main__":
     main()
