ad814094f1696563b168525e604a9907a4283051
PaperDrago <charlie_shadow@hotmail.com>
2024-11-04 16:55:30 -0600

    made main.py more legible and fixed logic errors with .containers

--- main.py
+++ main.py
@@ -4,23 +4,36 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroids
+from asteroidfield import AsteroidField
 
 def main():
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-    print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    updateable = pygame.sprite.Group()
+
+    updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroids.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+    print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        player.draw(screen)
-        player.update(dt)
+        for group1 in drawable:
+            group1.draw(screen)
+        for group2 in updatable:
+            group2.update(dt)
         pygame.display.flip()
         dt = clock.tick(60)/1000
     
