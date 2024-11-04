3ca4949840e25399b442da71b4a2027172b8de0d
PaperDrago <charlie_shadow@hotmail.com>
2024-11-04 09:21:34 -0600

    added base of sprite code

--- main.py
+++ main.py
@@ -3,18 +3,24 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    i = 0
-    while i == 0:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.fill((0,0,0))
+        screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+    
 
 if __name__ == "__main__":
     main()
