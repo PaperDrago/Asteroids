8ec8c1dddbf41382246f6da84f01bec41a0c3217
PaperDrago <charlie_shadow@hotmail.com>
2024-11-04 09:23:37 -0600

    Fixed import mistakes

--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+import pygame
 from constants import *
 from circleshape import CircleShape
 
@@ -16,5 +17,5 @@ class Player(CircleShape):
         return [a, b, c]
     
     def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        pygame.draw.polygon(screen, (255,255,255), self.triangle(), 2)
         
